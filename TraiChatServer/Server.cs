1b8ef04e163f01f9e5e13e2e2513747daa82e9c8
Traijan <weberalexander00@web.de>
2021-01-22 15:31:34 +0100

    Erster Informationenaustausch mit Client

--- TraiChatServer/Server.cs
+++ TraiChatServer/Server.cs
@@ -12,15 +12,21 @@ namespace TraiChatServer {
 
         static Socket serverSocket;
         const int port = 7777;
+        static byte[] buf;
 
         public static String Name { get { return _name; } }
 
         public static void InitServer() {
+            _name = "Traijan's Verrückter Server";
+            buf = new byte[4096];
+            _connectedClients = new List<Client>();
+            
             // Server starten
             serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             serverSocket.Bind(new IPEndPoint(IPAddress.Any, port));
             serverSocket.Listen(2); // 2 gleichzeitig zum accepten, alle andere in einer Warteschlange
-            serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null); 
+            serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+
 
             // Chats laden
             LoadChats();
@@ -33,13 +39,46 @@ namespace TraiChatServer {
         static void AcceptCallback(IAsyncResult ar) {
             Socket socket = serverSocket.EndAccept(ar);
             serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+            socket.BeginReceive(buf, 0, buf.Length, SocketFlags.None, ReceiveCallback, socket);
 
-            Console.WriteLine("Socket: " + socket.ToString() + " hat sich verbunden");
+            var message = new SocketMessage(MessageType.Verify);
+            message.AddHeaderData("name", _name); // Send things like Server Name, Online Users, etc
+            socket.Send(message.ToJSONBytes());
+
             //AddClient(new Client());
         }
 
-        public static void AddClient(Client client) {
-            _connectedClients.Add(client);
+        static void ReceiveCallback(IAsyncResult ar) {
+            Socket sock = (Socket)ar.AsyncState;
+            int recv = 0;
+
+            try {
+                recv = sock.EndReceive(ar); 
+            }
+            catch {
+                // Find Client with sock als Socket and disconnect him properly
+                return;
+            }
+
+
+            sock.BeginReceive(buf, 0, buf.Length, SocketFlags.None, ReceiveCallback, sock);
+
+            byte[] data = new byte[recv];
+            Array.Copy(buf, data, recv);
+
+            SocketMessage m = SocketMessage.FromJSONBytes(data);
+
+            switch(m.MessageType) {
+                case MessageType.Verify:
+                    AddClient(m);
+                    break;
+            }
+        }
+
+        public static void AddClient(SocketMessage m) {
+            _connectedClients.Add(new Client());
+
+            Console.WriteLine("[Neue Verbindung] Client besitzt die ID: " + m.Header["id"] + " mit dem Namen: " + m.Header["name"]);
         }
     }
 }
---
+++ TraiChatServer/SocketMessage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace TraiChatServer {
+    [Serializable]
+    public enum MessageType {
+        Verify, // Server sends Verify-Message to Client and Client answers with Verify-Message
+
+    }
+
+    [Serializable]
+    public class SocketMessage {
+        public Dictionary<String, String> Header { get; private set; }
+        public MessageType MessageType { get; private set; }
+
+        public SocketMessage(MessageType type) {
+            Header = new Dictionary<String, String>();
+            MessageType = type;
+        }
+
+        public void AddHeaderData(String key, String data) {
+            Header.Add(key, data);
+        }
+
+        public byte[] ToJSONBytes() {
+            return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(this));
+        }
+
+        public static SocketMessage FromJSONBytes(byte[] json) {
+            return JsonConvert.DeserializeObject<SocketMessage>(Encoding.UTF8.GetString(json));
+        }
+    }
+}
